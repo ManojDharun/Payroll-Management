a8bc6faa21d923fb46fb40879bf69422ad2b5089
ManojDharun <104717205+ManojDharun@users.noreply.github.com>
2023-06-30 17:56:57 +0530

    Add files via upload

---
+++ MasterData.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class MasterData{
+    private ArrayList<Employee> employeeslist = new ArrayList<Employee>();
+    public MasterData(ArrayList<Employee> employee){
+        this.employeeslist = employee;
+    }
+    public ArrayList<Employee> getEmployeeList(){
+        return this.employeeslist;
+    }
+}
